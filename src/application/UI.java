e7de618a174e1c2af2fa143ca85a573585bc8985
Vitor <vitorolivera2009@gmail.com>
2024-07-16 16:04:35 -0300

    Possible moves of piece

--- src/application/UI.java
+++ src/application/UI.java
@@ -41,7 +41,7 @@ public class UI {
 			return new ChessPosition(column, row);
 		}
 		catch(RuntimeException e ) {
-			throw new InputMismatchException("Error reading ChessPosition. Vlalid values are from a1 to h8.");
+			throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
 		}
 	}
 	
